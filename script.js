b1f1264e88b9fc706b0bda83947dd06f63427f93
Daniel Ferreira <d2fgithub@gmail.com>
2024-07-10 11:55:58 -0300

    Adição de uma função para declarar um ganhador no Pedra, Papel e Tesoura.

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 /* Desenvolva seu código abaixo */
-console.log("Primeira entrega")
+function playRockPaperScissor(player1, player2) { 
+
+    if (player1 == "Pedra" && player2 == "Tesoura" || player1 == "Papel" && player2 == "Pedra" || player1 == "Tesoura" && player2 == "Papel") {
+        return "Jogador 1 venceu!"
+    } else if (player2 == "Pedra" && player1 == "Tesoura" || player2 == "Papel" && player1 == "Pedra" || player2 == "Tesoura" && player1 == "Papel") {
+        return "Jogador 2 venceu!"
+    } else {
+        return "Empate!"
+    }
+}
